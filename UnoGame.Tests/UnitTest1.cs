ebe96e220b2ef4a0e07605e3d68f88d9a6f80f66
Cahayani Dinda Permatasari <cahayanidindapermatasari@gmail.com>
2026-07-02 10:11:07 +0700

    add: UnoGame.Test

---
+++ UnoGame.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace UnoGame.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
